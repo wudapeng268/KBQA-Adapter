98c720b5d7377210c89be1bbc66de36aec34a32a
wudapeng268 <562321553@qq.com>
2019-09-29 09:53:34 +0800

    change baseline-star.sh to load corresponding baseline when run adapter star model

--- qa+adapter/script/baseline-star.sh
+++ qa+adapter/script/baseline-star.sh
@@ -6,7 +6,7 @@ export CUDA_VISIBLE_DEVICES=$1
 for i in {0..9}
 do
     python -u start.py --run train --config config/baseline-star.yml --star_model --fold $i | tee baseline-star.yml.log
-    python -u start.py --run train --config config/gan-recon-all-star.yml --fold $i | tee gan.recon.all.star.yml.log
+    python -u start.py --run train --config config/gan-recon-all-star.yml --star_model --fold $i | tee gan.recon.all.star.yml.log
 done
 
 echo "all done!"
